96a06ef9351a6f17eefe2ebf0e0557806e21cb70
KayMel98 <108762927+KayMel98@users.noreply.github.com>
2022-11-05 11:51:04 -0700

    Create initials.cpp
    Outputs creators initials in format specified.

---
+++ initials.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+  // Output Initials KM in block form
+  std::cout << "K   K M   M\n";
+  std::cout << "K  K  MM MM\n";
+  std::cout << "K K   M M M\n";
+  std::cout << "K     M   M\n";
+  std::cout << "K K   M   M\n";
+  std::cout << "K  K  M   M\n";
+  std::cout << "K   K M   M\n";
+}
